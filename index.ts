6e0cadeba5d6e7cdbda782fe593e32f0a60854b6
Io Klarstrup <io@klarstrup.dk>
2025-01-25 23:25:36 +0100

    Initialize GoPro video processing project with TypeScript setup and dependencies

---
+++ index.ts
@@ -0,0 +1,216 @@
+#!/usr/bin/env -S npx tsx
+
+import { copyFile } from "copy-file";
+import ffprobe from "ffprobe-client";
+import ffmpeg from "fluent-ffmpeg";
+import { Stats } from "fs";
+import fs from "fs/promises";
+import { GoProTelemetry } from "gopro-telemetry";
+import Multiprogress from "multi-progress";
+
+const extractGPMF = async (videoFile: string) => {
+  for (const stream of (await ffprobe(videoFile)).streams) {
+    if (stream.codec_tag_string !== "gpmd") continue;
+
+    return extractGPMFAt(videoFile, stream.index);
+  }
+};
+function timemarkToSeconds(timemark: string) {
+  if (typeof timemark === "number") {
+    return timemark;
+  }
+
+  if (timemark.indexOf(":") === -1 && timemark.indexOf(".") >= 0) {
+    return Number(timemark);
+  }
+
+  var parts = timemark.split(":");
+
+  // add seconds
+  var secs = Number(parts.pop());
+
+  if (parts.length) {
+    // add minutes
+    secs += Number(parts.pop()) * 60;
+  }
+
+  if (parts.length) {
+    // add hours
+    secs += Number(parts.pop()) * 3600;
+  }
+
+  return secs;
+}
+
+const extractGPMFAt = async (
+  videoFile: string,
+  stream: number,
+  buffer: Buffer<ArrayBuffer> = Buffer.alloc(0)
+) =>
+  new Promise<Buffer<ArrayBuffer>>((resolve, reject) =>
+    ffmpeg(videoFile)
+      .outputOption("-y")
+      .outputOptions("-codec copy")
+      .outputOptions(`-map 0:${stream}`)
+      .outputOption("-f rawvideo")
+      .on("error", reject)
+      .pipe()
+      .on("data", (chunk) => (buffer = Buffer.concat([buffer, chunk])))
+      .on("end", () => resolve(buffer))
+  );
+
+async function getDeviceNameFromFile(videoPath: string): Promise<string> {
+  const telemetry = await GoProTelemetry(
+    { rawData: await extractGPMF(videoPath) },
+    { raw: true }
+  );
+
+  // @ts-expect-error - The type definitions are wrong
+  const DEVC = Array.isArray(telemetry.DEVC)
+    ? // @ts-expect-error - The type definitions are wrong
+      telemetry.DEVC[0]
+    : // @ts-expect-error - The type definitions are wrong
+      telemetry.DEVC;
+
+  return DEVC.DVNM;
+}
+
+const sessionName = `2025-02-24-Io-Drumming`;
+const destinationFolder = `/Volumes/@klarstrup2/${sessionName}`;
+
+throw new Error("Not implemented");
+
+const multi = new Multiprogress();
+console.time("Total time");
+console.time("Scan time");
+console.log("Scanning for videos...");
+const videosByCamera: Record<
+  string,
+  {
+    videos: string[];
+  }
+> = {};
+await Promise.all(
+  (
+    await fs.readdir("/Volumes/")
+  ).map(async (volume) => {
+    if (!volume.startsWith("Untitled")) return;
+
+    const dirs = await fs.readdir(`/Volumes/${volume}/DCIM`);
+    await Promise.all(
+      dirs.map(async (dir) => {
+        if (!dir.startsWith("100")) return;
+
+        const dirFiles = await fs.readdir(`/Volumes/${volume}/DCIM/${dir}`);
+
+        let newestFile: { mtimeMs: number; file: string } | null = null;
+        for (const file of dirFiles) {
+          if (!file.endsWith(".MP4")) continue;
+          const filePath = `/Volumes/${volume}/DCIM/${dir}/${file}`;
+
+          const stats = await fs.stat(filePath);
+          if (newestFile === null || stats.mtimeMs > newestFile.mtimeMs) {
+            newestFile = { mtimeMs: stats.mtimeMs, file: filePath };
+          }
+        }
+
+        console.log(`Getting device name for ${newestFile.file}`);
+        const cameraName = dir.includes("GOPRO")
+          ? (await getDeviceNameFromFile(newestFile.file)).replace(
+              /[^a-zA-Z0-9]/g,
+              ""
+            )
+          : "NikonZ30";
+
+        if (!videosByCamera[cameraName])
+          videosByCamera[cameraName] = { videos: [] };
+        for (const file of dirFiles) {
+          if (!file.endsWith(newestFile.file.slice(-8))) continue;
+
+          const filePath = `/Volumes/${volume}/DCIM/${dir}/${file}`;
+
+          videosByCamera[cameraName].videos.push(filePath);
+        }
+      })
+    );
+  })
+);
+console.timeEnd("Scan time");
+
+console.time("Copy/Concatenate time");
+console.log("Creating destination folder...");
+await fs.mkdir(destinationFolder, { recursive: true });
+console.log("Copying videos...");
+for (const [camera, { videos }] of Object.entries(videosByCamera)) {
+  const destinationFile = `${destinationFolder}/${sessionName}-${camera}.MP4`;
+  const destinationStat: Stats | null = await fs
+    .stat(destinationFile)
+    .catch(() => null);
+
+  if (videos.length === 1) {
+    const onlyVideo = videos[0];
+    const sourceStat = await fs.stat(onlyVideo);
+
+    const bar = multi.newBar(
+      `Copying ${destinationFile} [:bar] :percent :etas`,
+      { total: sourceStat.size }
+    );
+
+    if (destinationStat && sourceStat.size <= destinationStat.size) {
+      console.log(`Already copied ${destinationFile}`);
+      continue;
+    }
+
+    await copyFile(onlyVideo, destinationFile, {
+      onProgress: ({ writtenBytes, size }) => bar.update(writtenBytes / size),
+    });
+
+    continue;
+  }
+
+  let totalDuration = 0;
+  const ffmpegCommand = ffmpeg();
+  for (const video of videos) {
+    totalDuration += +(await ffprobe(video)).format.duration;
+    ffmpegCommand.input(video);
+  }
+
+  if (destinationStat) {
+    const destinationDuration = +(await ffprobe(destinationFile)).format
+      .duration;
+
+    if (~~destinationDuration === ~~totalDuration) {
+      console.log(`Already concatenated ${destinationFile}`);
+      continue;
+    }
+  }
+
+  if (process.env.DEBUG) {
+    console.log("ffmpeg " + ffmpegCommand._getArguments().join(" "));
+  }
+
+  let bar: ProgressBar;
+  await new Promise((resolve, reject) => {
+    ffmpegCommand
+      .on("progress", (progress) => {
+        if (!bar) {
+          bar = multi.newBar(
+            `Concatenating ${destinationFile} [:bar] :percent :etas`,
+            { total: totalDuration }
+          );
+        }
+
+        bar.update(timemarkToSeconds(progress.timemark) / totalDuration);
+      })
+      .on("end", () => {
+        bar.update(1);
+        bar.complete = true;
+        resolve(undefined);
+      })
+      .on("error", reject)
+      .mergeToFile(destinationFile, "/tmp");
+  });
+}
+
+console.timeEnd("Copy/Concatenate time");
+console.timeEnd("Total time");
